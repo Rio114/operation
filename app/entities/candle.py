09b4cdf52009ff1233cbc1e69ca99b302b8bf25c
Rio114 <nomuraryoji114@gmail.com>
2021-11-23 17:40:27 +0900

    misc

--- app/entities/candle.py
+++ app/entities/candle.py
@@ -9,8 +9,8 @@ class QuoteCurrentCandleModel:
 
 @dataclasses.dataclass
 class QuoteHistoricalCandleModel:
-    granularity: str  # ["S5", "M1", "M15", "H1", "D"]
-    instrument: str  # ["USD_JPY", "EUR_USD", "EUR_JPY"],
+    granularity: str
+    instrument: str
     price_type: str  # ["M", "A", "B"]
     quote_from: str  # yyyymmdd
     quote_to: str  # yyyymmdd
